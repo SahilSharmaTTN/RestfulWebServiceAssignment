21fb5e03574bcad0403bc37670ffe5f92c7e5eed
Sahil Sharma <sahil.sharma@tothenew.com>
2022-10-20 14:49:30 +0530

    first

---
+++ src/test/java/com/sahil/RestfulWebServiceAssignment/RestfulWebServiceAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sahil.RestfulWebServiceAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServiceAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
